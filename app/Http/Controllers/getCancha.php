ba0364c625d0c45bc8c125ef357e95b84012d1d1
fede90 <rojo.fede@gmail.com>
2015-03-18 19:40:24 -0300

    borro metodos que no se usan y cambio el return

--- app/Http/Controllers/getCancha.php
+++ app/Http/Controllers/getCancha.php
@@ -12,9 +12,9 @@ class getCancha extends Controller {
 
 
 	public function getCancha(Request $request){
+
 		$cancha = $request->input('term');
 		$canchas = Cancha::all();
-
 		$result = [];
 
 		foreach ($canchas as $sede) {
@@ -22,83 +22,9 @@ class getCancha extends Controller {
 				$result[] = $sede;
 			}
 		}
-
-		return response()->json(["canchas"=>$result]);;
+		return $result[0];
 	}
 
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		//
-	}
-
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		//
-	}
-
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		//
-	}
-
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
-	}
-
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
-
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
-
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
-
+	
 }
