8a9f0feabd8b5d8b4327125c057f0120e8d65d01
fede90 <rojo.fede@gmail.com>
2015-03-19 18:45:56 -0300

    devuelvo un array de nombres

--- app/Http/Controllers/getCancha.php
+++ app/Http/Controllers/getCancha.php
@@ -6,25 +6,31 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Cancha;
 use Illuminate\Support\Str;
-use Illuminate\Http\Response;
+
 class getCancha extends Controller {
 
-
-
+//TODO: mejorar para que devuelva id/valor.  https://www.youtube.com/watch?v=pLBtduvx5b0
+//Así cuando se crea el partido se tiene el id y no hay que ir a buscarlo.
 	public function getCancha(Request $request){
 
 		$cancha = $request->input('term');
 		$canchas = Cancha::all();
-		$result = [];
-
+		$results = [];
 		foreach ($canchas as $sede) {
-			if(strpos(Str::lower($sede),$cancha) !== false){
-				$result[] = $sede;
+			if(strpos((Str::lower($sede['nombre'])),(Str::lower($cancha))) !== false){
+				$results[] = $sede;
 			}
 		}
-		return $result[0];
+		return $results[0];
+
+		return [
+        "id"=>"1",
+        "nombre"=>"Serrano",
+        "direccion"=>"serrano 123",
+        "telefono"=>"1234567",
+        "created_at"=>"-0001-11-30 00:00:00",
+        "updated_at"=>"-0001-11-30 00:00:00"
+		];
+
 	}
-
-
-	
 }
