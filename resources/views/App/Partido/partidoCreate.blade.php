66831eb4b76a455967d1a2534f436695d2c19e47
federico <rojo.fede@gmail.com>
2015-03-20 00:13:45 -0300

    modifico clase del buttom

--- resources/views/App/Partido/partidoCreate.blade.php
+++ resources/views/App/Partido/partidoCreate.blade.php
@@ -76,7 +76,7 @@
 
 						<div class="form-group">
 							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary">
+								<button type="submit" class="btn btn-primary form-control">
 									Crear
 								</button>
 							</div>
