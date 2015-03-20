4621b60ca7210256edc07c0330968628ab4ffdb2
fede90 <rojo.fede@gmail.com>
2015-03-20 19:22:10 -0300

    agrego vista de edición del partido

---
+++ resources/views/App/Partido/partidoEdit.blade.php
@@ -0,0 +1,54 @@
+@extends('app')
+
+@section('head')
+	<title> Fulbacho - Editar Partidos</title>
+@stop
+
+
+@section('content')
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2">
+			<div class="panel panel-default">
+				<div class="panel-heading">Nuevo</div>
+				<div class="panel-body">
+					@if (count($errors) > 0)
+						<div class="alert alert-danger">
+							<strong>Whoops!</strong> There were some problems with your input.<br><br>
+							<ul>
+								@foreach ($errors->all() as $error)
+									<li>{{ $error }}</li>
+								@endforeach
+							</ul>
+						</div>
+					@endif
+				<!--	{!!$partido->cancha!!} -->
+
+					{!!Form::model($partido,["method" => "PATCH","action" => ["PartidoController@update", $partido->id], 'class'=>'form-horizontal'])!!}
+						<!--Incluyo el formulario de edicion -->
+						@include('App/Partido/partidoForm')
+					{!!Form::close()!!}
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+
+@stop
+
+
+@section('script')
+
+
+	<script>
+		$( "#canchas" ).autocomplete({
+			source: 'http://localhost:8888/getCancha',
+			minLength:1,
+
+		});
+
+</script>
+
+@stop
