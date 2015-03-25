ae01288e798d3ebf59c95b57045c71f8aee68e55
federico <rojo.fede@gmail.com>
2015-03-24 23:48:44 -0300

    método para traer los contactos al autocomplete

---
+++ app/Http/Controllers/getContactos.php
@@ -0,0 +1,27 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Auth;
+
+class getContactos extends Controller {
+
+//TODO: mejorar para que devuelva id/valor.  https://www.youtube.com/watch?v=pLBtduvx5b0
+//Así cuando se crea el partido se tiene el id y no hay que ir a buscarlo.
+	public function getContactos(Request $request){
+
+		$contactoBusqueda = $request->input('term');
+		$userLogueado = Auth::user();
+		$contactos = $userLogueado->contactos()->get()->toArray();	
+		$results = [];
+		foreach ($contactos as $unContacto) {
+			if(strpos((Str::lower($unContacto['name'])),(Str::lower($contactoBusqueda))) !== false){
+				$results[] = $unContacto['name'];
+			}
+		}
+		return $results;
+	}
+}
