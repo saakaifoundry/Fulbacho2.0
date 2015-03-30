39a652c56efcc9f915e4a699e6b0250117a16627
fede90 <rojo.fede@gmail.com>
2015-03-30 19:49:09 -0300

    le voy poniendo estilo a la vista

--- resources/views/App/Partido/partidoShow.blade.php
+++ resources/views/App/Partido/partidoShow.blade.php
@@ -2,9 +2,21 @@
 
 @section('head')
 
-<title>Partido 1</title>
+<title>Partido {!!$partido->id!!}</title>
 @stop
 
 @section('content')
-	{!!$partido!!}
+    <div class="alert alert-success">
+    	<p>Acá podés ver toda la información del partido.</p>
+    	<p>Confirmar tu asistencia y ver la de tus amigos</p>
+	</div>
+
+
+	<p>	El partido se juega en: {!!$partido->sede->nombre!!} </p>
+
+	<p>	Los invitados son: {!!$partido->jugadores!!} </p>
+
+	<p>	Los Confirmados son: {!!$partido->confirmados!!} 
+
+	</p>
 @stop
