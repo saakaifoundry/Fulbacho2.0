33b085f9fc248aa5878522e73da8bd0666d31f89
federico <rojo.fede@gmail.com>
2015-04-01 00:12:59 -0300

    estilos a la vista del partido

--- resources/views/App/Partido/partidoShow.blade.php
+++ resources/views/App/Partido/partidoShow.blade.php
@@ -6,7 +6,7 @@
 @stop
 
 @section('content')
-    <div class="alert alert-success">
+    <div class="alert alert-info">
     	<p>Acá podés ver toda la información del partido.</p>
     	<p>Confirmar tu asistencia y ver la de tus amigos</p>
 	</div>
@@ -14,9 +14,17 @@
 
 	<p>	El partido se juega en: {!!$partido->sede->nombre!!} </p>
 
-	<p>	Los invitados son: {!!$partido->jugadores!!} </p>
+	<div>	Los invitados son: 
+    <ul class="list-group">
+    	@foreach ($partido->jugadores as $jugador)
+	        @if($jugador->id%2==0)
+		        <li class="list-group-item list-group-item-success"><span class="badge glyphicon glyphicon-remove-circle"> </span> {!!$jugador->name !!}</li>
+    		@else
+		        <li class="list-group-item list-group-item-warning"><span class="badge glyphicon glyphicon-ok-circle"> </span>{!!$jugador->name !!}</li>
+    		@endif
+		@endforeach
+    </ul>
 
-	<p>	Los Confirmados son: {!!$partido->confirmados!!} 
-
-	</p>
+	<p>{!!$partido->confirmados()->count()!!} confirmados</p>
+	<p>{!!$partido->jugadores()->count() - $partido->confirmados()->count()!!} no confirmados</p>
 @stop
